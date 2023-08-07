594c9982c7bc7eafaff3733e74d67f1ce330e15e
s1b0823 <sharansh.bagaria@walmart.com>
2023-08-08 02:07:34 +0530

    Added Signing and Verification of Block & Transaction. Made Generic Encoding and Hashing Function.

---
+++ core/encoding.go
@@ -0,0 +1,12 @@
+package core
+
+import "io"
+
+
+type Encoder[T any] interface{
+	Encode(*io.Writer, T) error
+}
+
+type Decoder[T any] interface{
+	Decode(*io.Reader, T) error
+}
--- core/transaction.go
+++ core/transaction.go
@@ -1,16 +1,35 @@
 package core
 
-import "io"
+import (
+	"fmt"
+
+	"github.com/sharansh123/MyBlockChain/crypto"
+)
+
 
 type Transaction struct{
-
+	Data []byte
+	PublicKey crypto.PublicKey
+	Signature *crypto.Signature
 }
 
+func (tx *Transaction) Sign(privKey crypto.PrivateKey) (error){
+	sig, err := privKey.Sign(tx.Data)
+	if err != nil{
+		return err
+	}
+	tx.PublicKey = privKey.PublicKey()
+	tx.Signature = sig
 
-func (tx *Transaction) DecodeBinary(r io.Reader) error{
 	return nil
 }
 
-func (tx *Transaction) EncodeBinary(w io.Writer) error{
+func (tx *Transaction) Verify() (error){
+	if tx.Signature == nil {
+		return fmt.Errorf("tx has no signature")
+	}
+	if !tx.Signature.Verify(tx.PublicKey, tx.Data){
+		return fmt.Errorf("Invalid Transaction")
+	}
 	return nil
 }
