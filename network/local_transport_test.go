2b5a6f233fb9dde89dba80ecf7cca9da31f332a5
s1b0823 <sharansh.bagaria@walmart.com>
2023-08-26 19:11:27 +0530

    Test cases for broadcast

--- network/local_transport_test.go
+++ network/local_transport_test.go
@@ -2,7 +2,6 @@ package network
 
 import (
 	"testing"
-
 	"github.com/stretchr/testify/assert"
 )
 
@@ -22,9 +21,29 @@ func TestSend(t *testing.T){
 	trb := NewLocalTransport("B").(*LocalTransport)
 	tra.Connect(trb)
 	trb.Connect(tra)
-
-	assert.Nil(t,tra.SendMessage(trb.addr,[]byte("hello")))
+	msg := []byte("hello")
+	assert.Nil(t,tra.SendMessage(trb.addr,msg))
 
 	rpc := <- trb.Consume()
-	assert.Equal(t, string(rpc.Payload), "hello")
+	b := make([]byte, len(msg))
+	rpc.Payload.Read(b)
+	assert.Equal(t,"hello",string(b))
+}
+
+func TestBroadcast(t *testing.T){
+	tra := NewLocalTransport("A").(*LocalTransport)
+	trb := NewLocalTransport("B").(*LocalTransport)
+	trc := NewLocalTransport("C").(*LocalTransport)
+	tra.Connect(trb)
+	tra.Connect(trc)
+	msg := []byte("Hello")
+	assert.Nil(t, tra.Broadcast(msg))
+	rpc_b := <- trb.Consume()
+	rpc_c := <- trc.Consume()
+	out_b := make([]byte, len(msg))
+	out_c := make([]byte, len(msg))
+	rpc_b.Payload.Read(out_b)
+	rpc_c.Payload.Read(out_c)
+	assert.Equal(t, msg, out_b)
+	assert.Equal(t, msg, out_c)
 }
