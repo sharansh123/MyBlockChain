fdb6150ed066f0b9bc3cbc10512b6444866d305f
s1b0823 <sharansh.bagaria@walmart.com>
2023-09-23 20:23:08 +0530

    Create Stack to store numbers and strings from Instruction. Added Sub & Pack Instructions

--- core/vm.go
+++ core/vm.go
@@ -6,30 +6,55 @@ import "fmt"
 type Instruction byte
 
 const (
-	InstrPush Instruction = 0x0a
+	InstrPushInt Instruction = 0x0a
 	InstrAdd Instruction = 0x0b
+	InstrPushByte Instruction = 0x0c
+	InstrPack Instruction = 0x0d
+	InstrSub Instruction = 0xe
 )
+
+type Stack struct{
+	data []any
+	sp int
+}
+
+func NewStack(size int) *Stack {
+	return &Stack{
+		data: make([]any, size),
+		sp:   0,
+	}
+}
+
+func (s *Stack) Push(v any){
+	s.data[s.sp] = v
+	s.sp++
+}
+
+func (s *Stack) Pop() any{
+	value := s.data[0]
+	s.data = s.data[1:]
+	s.sp--
+	return value
+}
 
 type VM struct {
 	data []byte
 	ip int
-	stack []byte
-	sp int //stack pointer
+	stack *Stack
 }
 
 func NewVM(data []byte) *VM {
 	return &VM{
 		data: data,
 		ip: 0,
-		stack: make([]byte, 1024),
-		sp: 0,
+		stack: NewStack(128),
 	}
 }
 
 func (vm *VM) Run() error{
 	for {
 		instr := vm.data[vm.ip]
-		fmt.Println(instr)
+		//fmt.Println(instr)
 
 		if err := vm.Exec(Instruction(instr)); err!=nil{
 			fmt.Errorf("Instruction Not Valid")
@@ -45,19 +70,34 @@ func (vm *VM) Run() error{
 
 func (vm *VM) Exec(instr Instruction) error {
 	switch(instr){
-	case InstrPush:
-		vm.pushStack(vm.data[vm.ip - 1])
+	case InstrPushByte:
+		vm.stack.Push(vm.data[vm.ip - 1])
+	case InstrPushInt:
+		vm.stack.Push(int(vm.data[vm.ip - 1]))
+	case InstrPack:
+		n := vm.stack.Pop().(int)
+		b := make([]byte, n)
+		for i :=0; i <n; i++{
+			b[i] = vm.stack.Pop().(byte)
+		}
+		vm.stack.Push(b)
+	case InstrSub:
+		a := vm.stack.Pop().(int)
+		b := vm.stack.Pop().(int)
+		c := a - b
+		vm.stack.Push(c)
+
 	case InstrAdd:
-		a := vm.stack[0]
-		b := vm.stack[1]
+		a := vm.stack.Pop().(int)
+		b := vm.stack.Pop().(int)
 		c := a + b
-		vm.pushStack(c)
+		vm.stack.Push(c)
 	}
 	return nil
 }
 
 
 func (vm *VM) pushStack(b byte){
-	vm.stack[vm.sp] = b
-	vm.sp++
+	//vm.stack[vm.sp] = b
+	//vm.sp++
 }
--- core/vm_test.go
+++ core/vm_test.go
@@ -1,14 +1,28 @@
 package core
 
 import (
+	"fmt"
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 )
 
 
 func TestVM(t *testing.T){
-	data := []byte{0x1, 0xa, 0x2, 0xa, 0xb}
+	data := []byte{0x2, 0xa, 0x61, 0xc, 0x61, 0xc, 0xd}
 	vm := NewVM(data)
 	assert.Nil(t,vm.Run())
-	assert.Equal(t, byte(3), vm.stack[2])
+	//fmt.Println(vm.stack.data)
+	result := vm.stack.Pop().([]byte)
+	assert.Equal(t, "aa", string(result))
+}
+
+func TestStack(t *testing.T){
+	st := NewStack(125)
+	st.Push(12)
+	st.Push(23)
+	st.Pop()
+	fmt.Printf("%+v", st.data)
+	st.Pop()
+	fmt.Printf("%+v", st.data)
 }
